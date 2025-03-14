408c693f1607b42c99bef33defb07c5b210a75b4
AE ( ͡ಠ ʖ̯ ͡ಠ) <36546967+AE-MS@users.noreply.github.com>
2025-03-14 08:59:43 -0700

    code

---
+++ public/script_from_ts.ts
@@ -0,0 +1,8 @@
+function alertUser() {
+  const secondParagraph = document.getElementById("2p");
+  if (secondParagraph === null) {
+    alert("Second paragraph not found!");
+  } else {
+    secondParagraph.innerText = "Hello CMNDO!";
+  }
+}
